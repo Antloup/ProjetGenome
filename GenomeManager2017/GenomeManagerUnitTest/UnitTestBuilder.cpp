36129debc6641e80fed329392cda042193f6f557
Augustin Gaillot <augustingaillot@gmail.com>
2017-05-24 14:28:07 +0200

    Projet OK pour tests

---
+++ GenomeManager2017/GenomeManagerUnitTest/UnitTestBuilder.cpp
@@ -0,0 +1,45 @@
+#include "stdafx.h"
+#include "../GenomeServer2017/stdafx.h"
+#include "CppUnitTest.h"
+#include "BuildRequestClient.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace GenomeServerUnitTest
+{
+	TEST_CLASS(UnitTestBuilder)
+	{
+	public:
+
+		string path_test_file = "../GenomeTest";
+		string string_test;
+
+		TEST_METHOD(TestMethodBuilderDiseases)
+		{
+			BuildRequestClient builder_test(path_test_file);
+
+			string_test = "MA v1.0\r\nGET DESEASES\r\n\r\n";
+			builder_test.requestDiseases();
+			Assert::AreEqual(string_test, builder_test.getRequest());
+		}
+
+		TEST_METHOD(TestMethodBuilderFullAnalysis)
+		{
+			BuildRequestClient builder_test(path_test_file);
+
+			string_test = "MA v1.0\r\CHECK ALL\r\AAAA;GGGG;TTTT;CCCC;\r\n\r\n";
+			builder_test.requestFullAnalysis();
+			Assert::AreEqual(string_test, builder_test.getRequest());
+		}
+
+		TEST_METHOD(TestMethodBuilderSpecificAnalysis)
+		{
+			BuildRequestClient builder_test(path_test_file);
+
+			string_test = "MA v1.0\r\CHECK DESEASE\r\nMaladieTest\r\nAAAA;GGGG;TTTT;CCCC;\r\n\r\n";
+			builder_test.requestSpecificAnalysis("MaladieTest");
+			Assert::AreEqual(string_test, builder_test.getRequest());
+		}
+
+	};
+}
